04f09cd8f85a0a41b0b8c3c1370cac401183b22c
DiogoFlowers <diogoflores2009@gmail.com>
2025-05-28 20:18:09 -0300

    add class Task

--- src/main/java/com/didi/todoapi/domain/Task.java
+++ src/main/java/com/didi/todoapi/domain/Task.java
@@ -1,4 +1,30 @@
 package com.didi.todoapi.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Task")
+
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String title;
+
+    private String description;
+
+    private boolean completed;
+
+    private LocalDateTime createdAt;
 }
