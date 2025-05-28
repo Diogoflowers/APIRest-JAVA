970c2aa7592d4057568ca42abab6699b4a118933
DiogoFlowers <diogoflores2009@gmail.com>
2025-05-28 20:25:57 -0300

    Add DTOS class

--- src/main/java/com/didi/todoapi/dto/TaskRequestDTO.java
+++ src/main/java/com/didi/todoapi/dto/TaskRequestDTO.java
@@ -1,4 +1,15 @@
 package com.didi.todoapi.dto;
 
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TaskRequestDTO {
+    @NotBlank(message = "O título é obrigatório")
+    private String title;
+
+    private String description;
 }
--- src/main/java/com/didi/todoapi/dto/TaskResponseDTO.java
+++ src/main/java/com/didi/todoapi/dto/TaskResponseDTO.java
@@ -1,4 +1,17 @@
 package com.didi.todoapi.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
 public class TaskResponseDTO {
+
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+    private LocalDateTime createdAt;
 }
