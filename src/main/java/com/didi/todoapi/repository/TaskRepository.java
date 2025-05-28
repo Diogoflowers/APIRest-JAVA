0819d1b6a59ac3b45995c4480ec202aa3ceb5d39
DiogoFlowers <diogoflores2009@gmail.com>
2025-05-28 20:21:05 -0300

    Create interface TaskRepository

--- src/main/java/com/didi/todoapi/repository/TaskRepository.java
+++ src/main/java/com/didi/todoapi/repository/TaskRepository.java
@@ -1,4 +1,7 @@
 package com.didi.todoapi.repository;
 
-public class TaskRepository {
+import com.didi.todoapi.domain.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
